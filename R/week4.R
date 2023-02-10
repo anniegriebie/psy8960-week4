c2acb34d32bf37cfe22daa3c399917cce41b1d33
anniegriebie <GRIEB046@umn.edu>
2023-02-10 17:18:26 -0600

    Separated Columns

--- R/week4.R
+++ R/week4.R
@@ -4,3 +4,5 @@ library(tidyverse)
 
 # Data Import
 import_tbl <- read_delim(file = "../data/week4.dat", delim="-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+glimpse(import_tbl)
+wide_tbl <- separate(import_tbl, qs, into = c("q1", "q2", "q3", "q4", "q5"), sep = " - ")
